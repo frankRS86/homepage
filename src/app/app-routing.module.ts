29fffff287f5a966601624993cba9710cfc199b1
Frank Siller <frank.siller@gmx.net>
2021-01-23 20:35:01 +0100

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const HOME = 'home';
+const LEARNING = 'learning'
+
+const routes: Routes = [
+  {path:'', redirectTo: `/${HOME}`,pathMatch:'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calisthenics-hero';
+
+  returnHome()
+  {
+    console.log("return home context");
+  }
 }
