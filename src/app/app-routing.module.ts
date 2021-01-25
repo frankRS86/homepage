db606d673b130276a2f291683d550274bb1f0325
ehdf6xl <frank.siller@porsche.de>
2021-01-25 11:55:26 +0100

    basic layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
 
 const HOME = 'home';
 const LEARNING = 'learning'
 
 const routes: Routes = [
   {path:'', redirectTo: `/${HOME}`,pathMatch:'full'},
+  { path: HOME, component: HomeScreenComponent },
+
 
 ];
 
